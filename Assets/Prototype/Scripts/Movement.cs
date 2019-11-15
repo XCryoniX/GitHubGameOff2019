e488eefdd978db263ebf472428dff151d31b7fe4
XCryoniX <oomeeth.p@gmail.com>
2019-11-15 22:29:05 +0200

    Third Person Movement For Sontag

--- Assets/Prototype/Scripts/Movement.cs
+++ Assets/Prototype/Scripts/Movement.cs
@@ -2,8 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public enum CameraType
+{
+    thirdPerson
+}
+
 public class Movement : MonoBehaviour
 {
+    [SerializeField]
+    private CameraType cameraType = CameraType.thirdPerson;
+
     [SerializeField]
     private float slowMo = 1.0f;
 
@@ -17,6 +26,15 @@ public class Movement : MonoBehaviour
     private float y;
 
     private Vector3 deltaRotation;
+
+    [SerializeField]
+    private new Transform camera;
+
+    [SerializeField]
+    private Transform cameraTarget;
+
+    [SerializeField]
+    private float characterYOffset;
 
     private void Awake()
     {
@@ -43,20 +61,26 @@ public class Movement : MonoBehaviour
         x = Input.GetAxis("Mouse X");
         y = Input.GetAxis("Mouse Y");
 
-        if (x != 0 || y != 0)
-        {
-            deltaRotation = new Vector3((-y) * Time.deltaTime * rotationSpeed, (x) * Time.deltaTime * rotationSpeed, 0.0f);
-            GetComponent<Rigidbody>().MoveRotation(Quaternion.Euler(deltaRotation + GetComponent<Rigidbody>().rotation.eulerAngles));
-        }
-
         if (Input.GetKey(KeyCode.W))
         {
+            GetComponent<Animator>().SetBool("Idle", false);
+            GetComponent<Animator>().SetBool("RunForward", true);
             GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + (transform.forward * Time.deltaTime * movementSpeed));
+            transform.eulerAngles = new Vector3(0.0f, camera.localEulerAngles.y + characterYOffset, 0.0f);
+        }
+        else
+        {
+            GetComponent<Animator>().SetBool("RunForward", false);
+            GetComponent<Animator>().SetBool("Idle", true);
         }
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
             GetComponent<Rigidbody>().AddForce(transform.position + new Vector3(0.0f, 200.0f, 0.0f), ForceMode.Impulse);
         }
+
+        camera.position = cameraTarget.position + camera.TransformDirection(new Vector3(0.0f, 0.0f, -20.0f));
+        camera.position += camera.TransformDirection(new Vector3(-x, -y, 0.0f));
+        camera.LookAt(cameraTarget.position, Vector3.up);
     }
 }
